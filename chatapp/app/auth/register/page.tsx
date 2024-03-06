c57a5e52bf35ec760767f397208e25c5ffd879b0
alicend <a.taiki623@gmail.com>
2024-03-06 23:15:42 +0900

    EmailとPasswordの入力欄とヴァリデーションを実装

--- chatapp/app/auth/register/page.tsx
+++ chatapp/app/auth/register/page.tsx
@@ -1,20 +1,86 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    // await createUserWithEmailAndPassword(auth, data.email, data.password)
+    //   .then((userCrendential) => {
+    //     const user = userCrendential.user;
+    //     router.push("/auth/login");
+    //   })
+    //   .catch((error) => {
+    //     // console.error(error);
+    //     // alert(error);
+    //     if (error.code === "auth/email-already-in-use") {
+    //       alert("このメールアドレスはすでに使用されています。");
+    //     } else {
+    //       alert(error.message);
+    //     }
+    //   });
+  };
+
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <form className="bg-white p-8 rounded-lg shadow-md w-95">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white p-8 rounded-lg shadow-md w-96"
+      >
         <h1 className="mb-4 text-2xl text-gray-700 font-medium">新規登録</h1>
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-600">
             Email
           </label>
-          <input type="text" className="mt-1 border-2 rounded-md w-full p-2"/>
+          <input
+            {...register("email", {
+              required: "メールアドレスは必須です。",
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
+                message: "不適切なメールアドレスです。",
+              },
+            })}
+            type="text"
+            className="mt-1 border-2 rounded-md w-full p-2"
+          />
+          {errors.email && (
+            <span className="text-red-600 text-sm">{errors.email.message}</span>
+          )}
         </div>
         <div className="mb-4">
-          <label>Password</label>
-          <input type="password" className="mt-1 border-2 rounded-md w-full p-2"/>
+        <label className="block text-sm font-medium text-gray-600">
+            Password
+          </label>
+          <input
+            type="password"
+            {...register("password", {
+              required: "パスワードは必須です。",
+              minLength: {
+                value: 6,
+                message: "6文字以上入力してください。",
+              },
+            })}
+            className="mt-1 border-2 rounded-md w-full p-2"
+          />
+          {errors.password && (
+            <span className="text-red-600 text-sm">
+              {errors.password.message}
+            </span>
+          )}
         </div>
 
         <div className="flex justify-end">
